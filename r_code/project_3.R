c562dc161b4c798ac7e12de83b0edc2b98cf8a76
YeZo2023 <yezo2019@gmail.com>
2024-06-14 15:43:23 +0930

    submit

--- r_code/project_3.R
+++ r_code/project_3.R
@@ -1,6 +1,9 @@
 #Load the pwr package
 library(pwr)
-
+library(gridExtra)
+library(grid)
+library(png)
+library(ragg)
 # Calculate the effect size
 R2 <- 0.1
 f2 <- R2 / (1 - R2)
@@ -18,3 +21,12 @@ result <- pwr.f2.test(u = u, f2 = f2, sig.level = alpha, power = power)
 # Total sample size
 sample_size <- result$v + u + 1
 print(paste("Required sample size:", ceiling(sample_size)))
+
+
+# Create a table grob (graphical object)
+table_grob <- tableGrob(sample_size_table)
+
+# Save the table as a PNG image
+agg_png("tabs/sample_size_table.png", width = 1500, height = 400, res = 144)
+grid.draw(table_grob)
+dev.off()
